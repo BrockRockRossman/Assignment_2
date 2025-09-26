dfd61b8ac461a2282ff9b6dc4d3f283270b825d9
brockrossman <brock.rossman@gmail.com>
2025-09-26 16:40:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.assignment2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+    FragmentManager fg;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            fg = getSupportFragmentManager();
+            FragmentTransaction trans = fg.beginTransaction();
+            TickerListFragment tl = new TickerListFragment();
+            InfoWebFragment iw = new InfoWebFragment();
+            trans.add(R.id.colorFragment, tl, "colorFrag");
+            trans.add(R.id.colorListFragment, iw, "listFrag");
+            trans.commit();
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
